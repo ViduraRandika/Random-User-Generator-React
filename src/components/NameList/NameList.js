567e0ebd5277edad5be0c8c86fd149959a7c35d7
dilshan <dilshanudawatta15@gmail.com>
2021-11-30 20:33:43 +0530

    NameList.js file is created

---
+++ src/components/NameList/NameList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NameList(){
+    return(
+        <React.Fragment>
+            <h1>Name List</h1>
+            <ul>
+                <li>Dilshan - CST</li>
+                <li>Mahesh - IIT</li>
+                <li>Idunil - MRT</li>
+                <li>Asmika - SCT</li>
+            </ul>
+        </React.Fragment>
+    );
+}
+
+export default NameList;
