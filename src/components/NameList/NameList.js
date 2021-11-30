a5f9b404cfdde84d27e8f91411800621d0dfbecd
dilshan <dilshanudawatta15@gmail.com>
2021-11-30 21:04:17 +0530

    NameListItem.js file is created

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -1,14 +1,12 @@
 import React from "react";
+import NameListItem from "./NameListItem";
 
 function NameList(){
     return(
         <React.Fragment>
             <h1>Name List</h1>
             <ul>
-                <li>Dilshan - CST</li>
-                <li>Mahesh - IIT</li>
-                <li>Idunil - MRT</li>
-                <li>Asmika - SCT</li>
+                <NameListItem/>
             </ul>
         </React.Fragment>
     );
---
+++ src/components/NameList/NameListItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NameListItem(){
+    return(
+        <li>Dilshan - CST</li>
+    );
+}
+
+export default NameListItem;
