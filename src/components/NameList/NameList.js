3120efc469cc1e9a07214c8cb645a19202ec4c4b
dilshan <dilshanudawatta15@gmail.com>
2021-11-30 23:17:18 +0530

    Created NameList object

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -2,6 +2,26 @@ import React from "react";
 import NameListItem from "./NameListItem";
 
 function NameList(){
+
+    const NameList = {
+        "name": {
+            "title": "mr",
+            "first": "brad",
+            "last": "gibson"
+          },
+        "gender": "male",
+        "city": "kilcoole",
+        "email": "brad.gibson@example.com",
+        "dob": {
+            "date": "1993-07-20T09:44:18.674Z",
+            "age": 26
+          },
+          "phone": "011-962-7516",
+          "picture": {
+            "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+          },
+    };
+
     return(
         <React.Fragment>
             <h1>Name List</h1>
