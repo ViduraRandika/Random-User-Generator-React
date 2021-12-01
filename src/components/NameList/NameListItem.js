db6d0299a92f841bdb7b252c52a8614ad7897f5b
dilshan <dilshanudawatta15@gmail.com>
2021-12-02 01:15:19 +0530

    Set green color to Gender text

--- src/components/NameList/NameListItem.js
+++ src/components/NameList/NameListItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 import moment from "moment";
 
+import './NameListItem.css';
+
 function NameListItem(props){
     return(
         <li>
             <p><img src={props.dp} alt="This is dp"></img></p>
             <p style={{'color':'red'}}>Name : {props.name}</p>
-            <p>Gender : {props.gender}</p>
+            <p className="greenText">Gender : {props.gender}</p>
             <p>City : {props.city}</p>
             <p>Email : {props.email}</p>
             <p>DOB : {moment(props.dob).format('DD-MM-YYYY')} and {props.age} years old</p>
