e4b70964beccc2f05be57a599bf480e857e407c4
dilshan <dilshanudawatta15@gmail.com>
2021-12-27 01:31:25 +0530

    useState is applied

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -1,46 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import NameListItem from "./NameListItem";
 
 function NameList(){
-
-    const NameList = [{
+    const [NameList, setNameList] = useState([
+        {
         "id": 1,
         "name": {
             "title": "Mr",
             "first": "Brad",
             "last": "Gibson"
-          },
+            },
         "gender": "male",
         "city": "kilcoole",
         "email": "brad.gibson@example.com",
         "dob": {
             "date": "1993-07-20T09:44:18.674Z",
             "age": "28"
-          },
-          "phone": "011-962-7516",
-          "picture": {
+            },
+            "phone": "011-962-7516",
+            "picture": {
             "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-          },
-    },
-    {
+            },
+        },
+        {
         "id": 2,
         "name": {
             "title": "Mr",
             "first": "Clifford",
             "last": "Bell"
-          },
+            },
         "gender": "male",
         "city": "Westminster",
         "email": "clifford.bell@example.com",
         "dob": {
             "date": "1996-05-11T08:58:31.195Z",
             "age": "25"
-          },
-          "phone": "011-962-5516",
-          "picture": {
+            },
+            "phone": "011-962-5516",
+            "picture": {
             "medium": "https://randomuser.me/api/portraits/med/men/94.jpg",
-          },
-    }];
+            },
+        },
+        {
+        "id": 3,
+        "name": {
+            "title": "Mr",
+            "first": "Clifford",
+            "last": "Bell"
+            },
+        "gender": "male",
+        "city": "Westminster",
+        "email": "clifford.bell@example.com",
+        "dob": {
+            "date": "1996-05-11T08:58:31.195Z",
+            "age": "25"
+            },
+            "phone": "011-962-5516",
+            "picture": {
+            "medium": "https://randomuser.me/api/portraits/med/men/94.jpg",
+            },
+        }]
+    );
 
     const NameListComponent = () =>{
         return NameList.map((aName) => {
@@ -60,10 +80,39 @@ function NameList(){
         });
     };
 
+    const addUserHandler = () => {
+        const newUser =
+            {
+            "id": 1,
+            "name": {
+                "title": "Mr",
+                "first": "Brad",
+                "last": "Gibson"
+                },
+            "gender": "male",
+            "city": "kilcoole",
+            "email": "brad.gibson@example.com",
+            "dob": {
+                "date": "1993-07-20T09:44:18.674Z",
+                "age": "28"
+                },
+                "phone": "011-962-7516",
+                "picture": {
+                "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+                },
+            }
+
+        //setNameList((NameList) => NameList.concat(newUser));
+
+        //this is spread operator
+        setNameList((NameList) => [...NameList, newUser]);
+     }
+
     //mt-4 means margin top 4
     return(
         <React.Fragment>
             <div className="container mt-4">
+                <button className="btn btn-primary mb-2" onClick={addUserHandler}>Add Name</button>
                 <ul className="list-group">{NameListComponent()}</ul>
             </div>
         </React.Fragment>
