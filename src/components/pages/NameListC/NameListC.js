25917e82476a6e8bd3eb18d032bb3809fc39594c
dilshan <dilshanudawatta15@gmail.com>
2021-12-28 23:17:01 +0530

    class constructor method created

--- src/components/pages/NameListC/NameListC.js
+++ src/components/pages/NameListC/NameListC.js
@@ -1,9 +1,18 @@
 import React,{Component} from 'react';
 
 class NameListC extends Component{
+
+    constructor(){
+        super();
+        console.log("Constructor called!");
+    }
+
     render(){
         return(
-            <h1>Class Component</h1>
+            <div className="container mt-4">
+                <button className="btn btn-primary mb-2">Add Name</button>
+                <ul className="list-group"></ul>
+            </div>
         )
     }
 }
